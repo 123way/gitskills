f1c60b52d573b5ca2c033ffa7b538fa6ab60049a
尘梦如烟 <2764682630@qq.com>
2022-10-29 17:02:39 +0800

    Revert "颜子栋-第一次提交"
    This reverts commit 31437b1b5539014942579ba2ce956d3c0a84baba.

--- ISignUp.java
+++
@@ -1,8 +0,0 @@
-package 项目版本管理;
-
-public interface ISignUp {
-    public void print();
-    public boolean addStudent(int stuType);
-    //public static IParams parse() throws Exception{return null;};
-
-}
